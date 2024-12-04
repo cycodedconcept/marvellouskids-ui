d9723f7332807ae54cb5dd90e0db33a4ee72c488
Blossom Obasi <blossomobasi2@gmail.com>
2024-12-04 11:33:56 +0100

    Update Tailwind CSS configuration to add new color themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,20 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#007716",
+				lemon: "#FFFA00",
+				textColor: "#232323",
+				darkBlue: "#0D093B",
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;

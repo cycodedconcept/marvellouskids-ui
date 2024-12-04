d3d20d21d7c35b59ecbbde8e843626f60a2bd530
Blossom Obasi <blossomobasi2@gmail.com>
2024-12-04 20:13:50 +0100

    Implement mobile navigation for NavBar with toggle functionality

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -14,15 +14,17 @@ const navLinks = [
 
 const NavBar = () => {
 	const [activeNav, setActiveNav] = useState("/");
+	const [mobileNav, setMobileNav] = useState(false);
 	const pathname = usePathname();
 
 	return (
 		<header className="sticky top-0 bg-white z-50">
-			<div className="flex items-center justify-between px-14 py-3">
+			<div className="flex items-center justify-between md:px-14 px-5 py-3 bg-white">
 				<Logo />
 
-				<nav className="flex items-center space-x-5">
-					<ul className="flex items-center space-x-10">
+				{/* Desktop Navigation */}
+				<nav className="md:flex items-center space-x-5 hidden">
+					<ul className="flex items-center space-x-8">
 						{navLinks.map(({ href, name }) => (
 							<li
 								key={name}
@@ -46,6 +48,43 @@ const NavBar = () => {
 						</button>
 					</div>
 				</nav>
+
+				{/* Mobile Navigation */}
+				<nav
+					className={`absolute bg-white w-full left-0 p-5 -z-10 md:hidden flex transition-all duration-300 ease-in-out ${
+						mobileNav ? "top-20" : "-top-96"
+					}`}
+				>
+					<ul>
+						{navLinks.map((link) => (
+							<li
+								key={link.name}
+								className={`${
+									pathname === link.href && "bg-clip-text text-transparent bg-gradient-to-r from-primary to-lemon"
+								} py-2 text-lg font-semibold`}
+								onClick={() => {
+									setMobileNav(false);
+								}}
+							>
+								<Link href={link.href}>{link.name}</Link>
+							</li>
+						))}
+					</ul>
+				</nav>
+
+				{/* Mobile menu */}
+				<div onClick={() => setMobileNav((prev) => !prev)} className="flex flex-col space-y-2 cursor-pointer md:hidden">
+					<div
+						className={`h-1 w-8 rounded-full bg-primary transition-all duration-300 ${
+							mobileNav && "rotate-45 translate-y-2"
+						}`}
+					/>
+					<div
+						className={`h-1 w-8 rounded-full bg-primary transition-all duration-300 ${
+							mobileNav && "-rotate-45 -translate-y-1"
+						}`}
+					/>
+				</div>
 			</div>
 		</header>
 	);

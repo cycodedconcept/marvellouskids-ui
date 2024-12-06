e7616e35dc6bb25633e7dd73fb59e4f11330126c
Blossom Obasi <blossomobasi2@gmail.com>
2024-12-06 02:44:14 +0100

    Add sidebar content data for childcare and nephrology services

---
+++ data/sidebarContent.ts
@@ -0,0 +1,89 @@
+export const sidebarContentData = [
+	{
+		title: "Free Tax-Funded Childcare",
+		content: [
+			{ type: "img", src: "/assets/part-time-childcare.png", alt: "Intro Image" },
+			{ type: "h2", text: "Free Tax-Funded Childcare for 2, 3, and 4-Year-Olds" },
+			{
+				type: "p",
+				text: "Marvellous Kids Childcare offers tax-funded free childcare for eligible 2, 3, and 4-year-olds. This allows parents to take advantage of government support while ensuring their child receives high-quality care and early learning opportunities. Contact us to find out if you qualify and how to apply.",
+			},
+			{ type: "h5", text: "Enhancing the family experience" },
+			{
+				type: "p",
+				text: "We work to ensure our patients receive the highest standard of cancer care. All patients receive care guided by best practice standards of Cardio Care. These practices have been shown to provide the best patient outcomes. Patients and their families can quickly access a diverse range of cancer care services, including these facilities:",
+			},
+			{
+				type: "list",
+				items: [
+					"The BMO Breast Diagnostic Clinic, part of the Karen, Heather & Lynn Steinberg Breast Centre",
+					"The Baruch/Weisz Cardio Centre",
+					"Freeman Centre for the Advancement of Palliative Care",
+					"Gale and Graham Wright Prostate Centre",
+				],
+			},
+			{ type: "h5", text: "Parent supported team of experts" },
+			{
+				type: "p",
+				text: "Prettyhealth has one of the largest Cardio care programs in a community academic hospital in the GTA (Greater Toronto Area), and provides treatment for many types of CVD, including heart transplant, open heart surgery, engiogram prostate, gynecological and urinary.",
+			},
+			{
+				type: "p",
+				text: "Prettyhealth General is constantly thinking and going beyond for our patients, our community and our people. As people and communities change, Prettyhealth General will continuously evolve to meet their needs. We are driven to achieve the promise of people-centred care - to create a welcome and inclusive environment that contributes to health equity.",
+			},
+			{ type: "h5", text: "Information for new Kid" },
+			{
+				type: "p",
+				text: "Prettyhealth has one of the largest Cardio care programs in a community academic hospital in the GTA (Greater Toronto Area), and provides treatment for many types of CVD, including heart transplant, open heart surgery, engiogram prostate, gynecological and urinary.",
+			},
+			{
+				type: "p",
+				text: "Prettyhealth General is constantly thinking and going beyond for our patients, our community and our people. As people and communities change, Prettyhealth General will continuously evolve to meet their needs. We are driven to achieve the promise of people-centred care - to create a welcome and inclusive environment that contributes to health equity.",
+			},
+		],
+	},
+
+	{
+		title: "Medicine & Nephrology",
+		content: [
+			{ type: "img", src: "/assets/care-for-every-child.png", alt: "Intro Image" },
+			{ type: "h2", text: "Medicine & Nephrology" },
+			{
+				type: "p",
+				text: "Marvellous Kids Childcare offers tax-funded free childcare for eligible 2, 3, and 4-year-olds. This allows parents to take advantage of government support while ensuring their child receives high-quality care and early learning opportunities. Contact us to find out if you qualify and how to apply.",
+			},
+			{ type: "h5", text: "Enhancing the family experience" },
+			{
+				type: "p",
+				text: "We work to ensure our patients receive the highest standard of cancer care. All patients receive care guided by best practice standards of Cardio Care. These practices have been shown to provide the best patient outcomes. Patients and their families can quickly access a diverse range of cancer care services, including these facilities:",
+			},
+			{
+				type: "list",
+				items: [
+					"The BMO Breast Diagnostic Clinic, part of the Karen, Heather & Lynn Steinberg Breast Centre",
+					"The Baruch/Weisz Cardio Centre",
+					"Freeman Centre for the Advancement of Palliative Care",
+					"Gale and Graham Wright Prostate Centre",
+				],
+			},
+			{ type: "h5", text: "Parent supported team of experts" },
+			{
+				type: "p",
+				text: "Prettyhealth has one of the largest Cardio care programs in a community academic hospital in the GTA (Greater Toronto Area), and provides treatment for many types of CVD, including heart transplant, open heart surgery, engiogram prostate, gynecological and urinary.",
+			},
+			{
+				type: "p",
+				text: "Prettyhealth General is constantly thinking and going beyond for our patients, our community and our people. As people and communities change, Prettyhealth General will continuously evolve to meet their needs. We are driven to achieve the promise of people-centred care - to create a welcome and inclusive environment that contributes to health equity.",
+			},
+			{ type: "h5", text: "Information for new Kid" },
+			{
+				type: "p",
+				text: "Prettyhealth has one of the largest Cardio care programs in a community academic hospital in the GTA (Greater Toronto Area), and provides treatment for many types of CVD, including heart transplant, open heart surgery, engiogram prostate, gynecological and urinary.",
+			},
+			{
+				type: "p",
+				text: "Prettyhealth General is constantly thinking and going beyond for our patients, our community and our people. As people and communities change, Prettyhealth General will continuously evolve to meet their needs. We are driven to achieve the promise of people-centred care - to create a welcome and inclusive environment that contributes to health equity.",
+			},
+		],
+	},
+];

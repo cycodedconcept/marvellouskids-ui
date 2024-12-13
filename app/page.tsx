d928cf82399515633873427fcd39803e16ae3920
Blossom Obasi <blossomobasi2@gmail.com>
2024-12-13 14:17:31 +0100

    Refactor environment images to use external API for improved maintainability

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,12 @@
 import { Slide } from "@/animation/Slide";
 import AreaOfCare from "@/components/AreaOfCare";
 import ContactUs from "@/components/ContactUs";
+import { environment_API } from "@/data/ourEnvironment";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
-	const environment = [
-		{ src: "/assets/environment1.png", alt: "Environment 1" },
-		{ src: "/assets/environment2.png", alt: "Environment 2" },
-		{ src: "/assets/environment3.png", alt: "Environment 3" },
-		{ src: "/assets/environment4.png", alt: "Environment 4" },
-		{ src: "/assets/environment5.png", alt: "Environment 5" },
-		{ src: "/assets/environment6.png", alt: "Environment 6" },
-		{ src: "/assets/environment7.png", alt: "Environment 7" },
-		{ src: "/assets/environment8.png", alt: "Environment 8" },
-		{ src: "/assets/environment9.png", alt: "Environment 9" },
-		{ src: "/assets/environment10.png", alt: "Environment 10" },
-		{ src: "/assets/environment11.png", alt: "Environment 11" },
-		{ src: "/assets/environment12.png", alt: "Environment 12" },
-	];
+	const environment = environment_API;
 
 	return (
 		<div>

804807f183e2cb3840d12ad09203fdfe8d1155e3
Blossom Obasi <blossomobasi2@gmail.com>
2024-12-15 17:54:19 +0100

    Add custom hook useCountries for fetching and managing country data

---
+++ hook/useCountries.ts
@@ -0,0 +1,24 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export const useCountries = () => {
+	const [countries, setCountries] = useState<string[]>([]);
+
+	useEffect(() => {
+		const fetchCountries = async () => {
+			try {
+				const response = await fetch("https://restcountries.com/v3.1/all");
+				const data = await response.json();
+				const countryList = data.map((country: { name: { common: string; }; }) => country.name.common).sort();
+				setCountries(countryList);
+			} catch (error) {
+				console.error("Error fetching countries:", error);
+			}
+		};
+
+		fetchCountries();
+	}, []);
+
+	return countries;
+};

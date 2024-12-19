0836315ce0aaa9ba07485cc7c9fe34e1148b2bc1
Blossom Obasi <blossomobasi2@gmail.com>
2024-12-19 08:18:46 +0100

    Refactor Footer component: update links and restructure sections for improved clarity and navigation

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,31 +4,27 @@ import { FaInstagram, FaLinkedinIn, FaTwitter, FaYoutube } from "react-icons/fa"
 import { FaFacebookF } from "react-icons/fa6";
 
 const Footer = () => {
-	const products = [
-		{ title: "Pricing", href: "/" },
-		{ title: "Overview", href: "/" },
-		{ title: "Browse", href: "/" },
-		{ title: "Assisibility", href: "/" },
-	];
-
-	const solutions = [
-		{ title: "Brainstorming", href: "/" },
-		{ title: "Ideation", href: "/" },
-		{ title: "Wireframing", href: "/" },
-		{ title: "Research", href: "/" },
-	];
-
-	const resources = [
-		{ title: "Help Center", href: "/" },
-		{ title: "Blog", href: "/" },
-		{ title: 'Tutorials"', href: "/" },
-	];
-
 	const company = [
-		{ title: "About", href: "/" },
-		{ title: "Press", href: "/" },
-		{ title: "Events", href: "/" },
-		{ title: "Careers", href: "/" },
+		{ title: "Why Us", href: "/why-us" },
+		{ title: "Area Of Cares", href: "/area-of-care" },
+		{ title: "Nanny", href: "/nanny" },
+		{ title: "Our Enviroment", href: "/our-environment" },
+		{ title: "Our Solution", href: "/our-solution" },
+	];
+
+	const services = [
+		{ title: "Childcare Services", href: "/" },
+		{ title: "Nanny Services On Request", href: "/nanny" },
+		{ title: "Free Tax-Funded Childcare", href: "/" },
+		{ title: "Parent Resources", href: "/" },
+		{ title: "Helpful Article & Advice", href: "/" },
+	];
+
+	const more = [
+		{ title: "FAQ", href: "/faq" },
+		{ title: "Contact us", href: "/contact-us" },
+		{ title: "Blog", href: "/" },
+		{ title: "Help Center", href: "/" },
 	];
 
 	return (
@@ -38,7 +34,7 @@ const Footer = () => {
 				className="h-[25rem] bg-cover bg-no-repeat -mb-40 -mt-14"
 			/>
 			<div className="bg-darkBlue text-[#B4B3B4] md:px-14 px-5 py-5">
-				<div className="grid lg:grid-cols-6 md:grid-cols-3 grid-cols-2 gap-10 mb-8">
+				<div className="grid lg:grid-cols-5 grid-cols-2 gap-10 mb-8">
 					<div className="col-span-2">
 						<Logo type="light" />
 
@@ -47,17 +43,17 @@ const Footer = () => {
 						</h3>
 
 						<h3>
-							<span className="text-white">Opening & Closing Hours:</span> Sunday: 08:00 AM - 10:00 PM Monday - Friday:
-							06:00 AM - 12:00 AM
+							<span className="text-white">Opening & Closing Hours: </span>
+							Mon - Fri: 07:00AM - 06:00PM Sat - Sun 10:00AM - 04:00PM
 						</h3>
 					</div>
 
 					<ul>
 						<h3 className="text-white text-lg font-bold mb-5">Product</h3>
 						<div className="flex flex-col space-y-5">
-							{products.map((product, index) => (
-								<li key={index}>
-									<Link href={product.href}>{product.title}</Link>
+							{company.map((el, index) => (
+								<li key={index} className="hover:underline w-fit">
+									<Link href={el.href}>{el.title}</Link>
 								</li>
 							))}
 						</div>
@@ -66,9 +62,9 @@ const Footer = () => {
 					<ul>
 						<h3 className="text-white text-lg font-bold mb-5">Solutions</h3>
 						<div className="flex flex-col space-y-5">
-							{solutions.map((solution, index) => (
-								<li key={index}>
-									<Link href={solution.href}>{solution.title}</Link>
+							{services.map((el, index) => (
+								<li key={index} className="hover:underline w-fit">
+									<Link href={el.href}>{el.title}</Link>
 								</li>
 							))}
 						</div>
@@ -77,20 +73,9 @@ const Footer = () => {
 					<ul>
 						<h3 className="text-white text-lg font-bold mb-5">Resources</h3>
 						<div className="flex flex-col space-y-5">
-							{resources.map((resource, index) => (
-								<li key={index}>
-									<Link href={resource.href}>{resource.title}</Link>
-								</li>
-							))}
-						</div>
-					</ul>
-
-					<ul>
-						<h3 className="text-white text-lg font-bold mb-5">Company</h3>
-						<div className="flex flex-col space-y-5">
-							{company.map((company, index) => (
-								<li key={index}>
-									<Link href={company.href}>{company.title}</Link>
+							{more.map((el, index) => (
+								<li key={index} className="hover:underline w-fit">
+									<Link href={el.href}>{el.title}</Link>
 								</li>
 							))}
 						</div>

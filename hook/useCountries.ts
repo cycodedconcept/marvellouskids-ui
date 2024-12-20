0085bce49585748bf2bb47c97129f0883037897b
Blossom Obasi <blossomobasi2@gmail.com>
2024-12-20 09:08:58 +0100

    Update useCountries hook: add error handling for fetch request to improve reliability

--- hook/useCountries.ts
+++ hook/useCountries.ts
@@ -8,9 +8,10 @@ export const useCountries = () => {
 	useEffect(() => {
 		const fetchCountries = async () => {
 			try {
-				const response = await fetch("https://restcountries.com/v3.1/all", {
-					cache: "no-cache",
-				});
+				const response = await fetch("https://restcountries.com/v3.1/all");
+				if (!response.ok) {
+					throw new Error(`HTTP error! status: ${response.status}`);
+				}
 				const data = await response.json();
 				const countryList = data.map((country: { name: { common: string } }) => country.name.common).sort();
 				setCountries(countryList);

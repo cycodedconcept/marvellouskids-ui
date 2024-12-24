7680af620fd97c986ad769e2206ac97ffa984d40
Blossom Obasi <blossomobasi2@gmail.com>
2024-12-24 09:19:32 +0100

    Revert "Add new environment images and update Next.js configuration"
    This reverts commit ae49158df8ed7d49586340ead9aa7aa43f7e08e3.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	images: {
-		unoptimized: true,
-	},
-	output: "export",
+  /* config options here */
 };
 
 export default nextConfig;
